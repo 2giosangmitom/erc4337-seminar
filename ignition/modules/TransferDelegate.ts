7bdceccedcb9dc9c8a7c0178af58e3b83e6fff8a
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-06 17:08:19 +0700

    clean up

--- ignition/modules/TransferDelegate.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("TransferDelegate", (m) => {
-  // Deploy the demo contract used by the EIP-7702 authorization flow.
-  const transferDelegate = m.contract("TransferDelegate");
-
-  return { transferDelegate };
-});
